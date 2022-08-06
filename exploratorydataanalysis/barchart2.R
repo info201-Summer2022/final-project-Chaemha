c471bc798c3fe1aa402f485e629ae5bdcd295b5a
minolam <108096211+minolam@users.noreply.github.com>
2022-08-06 13:43:34 -0700

    Add files via upload

---
+++ exploratorydataanalysis/barchart2.R
@@ -0,0 +1,8 @@
+Barchart: 
+
+# Create the data for the chart
+A <- c(599, 7716, 8770)
+
+# Plot the bar chart 
+barchart <- barplot(A, xlab = "Elementary, Middle, High School", ylab = "Students of Hispanic Origin", col = "green", main ="Level of Educational Attainment for US Students of Hispanic Origin in 1995")
+
